23bc91cedfa409e6d231941fac311f667608947d
shwetakothari <shweta.a.kothari@gmail.com>
2018-04-21 21:34:01 -0700

    ui

--- Lab-3/ViewController.swift
+++ Lab-3/ViewController.swift
@@ -12,10 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navview.isHidden = true
+        
        
     }
 
-    
+    @IBAction func navButton(_ sender: Any) {
+        navview.isHidden = true
+        
+        
+        navview.bringSubview(toFront: navview)
 
+    }
+    
+    @IBOutlet var navview: UIView!
+    
+    
 }
 
